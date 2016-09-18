022db5075b022ca1d1f3df0485fb3924e09a8f95
luanxun <xun.luan@funplus.com>
2016-09-18 17:14:29 +0800

    put soft link in one dir

--- install.sh
+++ install.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
 PWD=`pwd`
-echo $PWD
+DOTFILES=`find . -name '\.*' -type f|xargs basename`
+BASHPROFILES=( .bash_profile .bashrc )
+NEEDSOURCE=(.alias .export .bash_funplus)
+HOME=`echo $HOME`
+
+for profile in ${BASHPROFILES[@]};do
+    if [ -f "${HOME}/${profile}" ];then
+        ETCFILE="${HOME}/${profile}"
+        break
+    fi
+done
+
+if [ -z ${ETCFILE} ];then
+    echo 'no bash etc file found'
+    exit
+else
+    echo "add dotfile to etc file:${ETCFILE}"
+fi
+for file in ${DOTFILES};do
+    FULLPATH=${PWD}/$file
+    ln -s -f ${FULLPATH} ${HOME}/${file} 
+    echo "source $file" >> ${ETCFILE}
+    
+done
