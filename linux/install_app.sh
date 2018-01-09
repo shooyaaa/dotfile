2499f4322b41b02d2414479a5190f508d3083d35
shooyaaa <shooyaaa@gmail.com>
2018-01-09 22:23:30 +0800

    fix

--- linux/install_app.sh
+++ linux/install_app.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install build-essential cmake vim
+sudo apt-get install build-essential cmake vim sysstat golang npm htop
+
+#swap caps and ctrl super as ctrl to copy and paste
+
+sed -i 's/XKBOPTIONS=""/XKBOPTIONS="ctrl:swapcaps,ctrl:swap_lwin_lctl"/g' /etc/default/keyboard
